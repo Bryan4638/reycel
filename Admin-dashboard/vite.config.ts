b7938753f0873fe9c1855ecf677950b85a596ce6
Bryan4638 <brayancespedes57@gmail.com>
2025-07-17 11:37:02 -0400

    update docker compose

--- Admin-dashboard/vite.config.ts
+++ Admin-dashboard/vite.config.ts
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
+    host: '0.0.0.0',  // Acepta conexiones externas
+    port: 5174,        // Puerto de Vite (opcional, ya que 5173 es el predetermin>
+    strictPort: true,   // Evita que Vite cambie el puerto si está ocupado
   },
+  preview: {
+    port: 4173,        // Puerto para el servidor de previsualización (build)
+    allowedHosts: [    // Dominios permitidos
+      'admin.reycel.com',
+      'localhost'
+    ]
+  }
 })
