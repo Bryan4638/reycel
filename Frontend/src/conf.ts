f0a28462718d4bd77f9e1bc95cb2fd43df7851a2
Bryan4638 <brayancespedes57@gmail.com>
2025-07-20 11:34:32 -0400

    update file workflows

--- Frontend/src/conf.ts
+++ Frontend/src/conf.ts
@@ -1,4 +1,4 @@
 // Environment variables in Vite must be prefixed with VITE_
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000';
-export const API_PYTHON_URL = import.meta.env.VITE_API_PYTHON_URL || 'http://localhost:8000';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://backend.reycel.com';
+export const API_PYTHON_URL = import.meta.env.VITE_API_PYTHON_URL || 'https://bot.reycel.com';
     
